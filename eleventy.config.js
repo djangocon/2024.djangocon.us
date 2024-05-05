7bafbb0c7469bb7d61e37e9509b9b38139ffb2bd
Michael Trythall <m@mtrythall.com>
2024-05-05 11:29:19 -0400

    Adds code for Atom feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,66 +3,12 @@ const path = require('path');
 const Image = require('@11ty/eleventy-img');
 const markdownIt = require("markdown-it");
 
+const setupCollections = require('./lib/collections');
+const setupFeed = require('./lib/feed');
+
 module.exports = (config) => {
-  /*
-    Setup collections
-    https://www.11ty.dev/docs/collections/
-  */
-  config.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/_content/posts/*.md");
-  });
-
-  config.addCollection("places", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/_content/places/*.md");
-  });
-
-  config.addCollection("presenters", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/_content/presenters/*.md");
-  });
-
-  config.addCollection("organizers", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/_content/organizers/*.md").sort(function(a, b) {
-        let nameA = a.data.name.toUpperCase();
-        let nameB = b.data.name.toUpperCase();
-        if (nameA < nameB) return -1;
-        else if (nameA > nameB) return 1;
-        else return 0;
-    }).filter(item => !item.data.hidden);
-  });
-
-  config.addCollection("sponsorsByLevel", function(collectionApi) {
-    const sponsors = collectionApi.getFilteredByGlob("src/_content/sponsors/*.md");
-    const visibleSponsors = sponsors.filter(sponsor => !sponsor.data.hidden);
-    const levelOrder = [
-      "Diamond",
-      "Platinum",
-      "Gold",
-      "Silver",
-      "Bronze",
-      "Coffee",
-      "Opportunity Grant",
-      "Community",
-    ];
-
-    const sponsorsByLevel = visibleSponsors.reduce((acc, sponsor) => {
-      const level = sponsor.data.level;
-      if (!acc[level]) {
-        acc[level] = [];
-      }
-      acc[level].push(sponsor);
-      return acc;
-    }, {});
-
-    // Sort levels based on predefined order
-    const sortedSponsorsByLevel = {};
-    levelOrder.forEach(level => {
-      if (sponsorsByLevel[level]) {
-        sortedSponsorsByLevel[level] = sponsorsByLevel[level];
-      }
-    });
-
-    return sortedSponsorsByLevel;
-  });
+  setupCollections(config);
+  setupFeed(config);
 
   /*
     Setup passthrough file copy
