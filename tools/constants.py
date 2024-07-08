9f32bba9584322689b404e0567e3cf437be1865f
Drew Winstel <drew@defna.org>
2024-07-08 13:45:52 -0500

    🐍 Add some basic constants to make coding easier

---
+++ tools/constants.py
@@ -0,0 +1,29 @@
+import datetime
+import json
+from pathlib import Path
+import zoneinfo
+
+SITE_JSON_FILE = Path(__file__).parent.parent / "src" / "_data" / "site.json"
+SITE_JSON = json.loads(SITE_JSON_FILE.read_text())
+CONFERENCE_TZ = zoneinfo.ZoneInfo(SITE_JSON["timezone"])
+CONFERENCE_YEAR = SITE_JSON["conf_year"]
+
+TUTORIAL_DAY = datetime.date(CONFERENCE_YEAR, 9, 22)
+TALK_DAY_1 = datetime.date(CONFERENCE_YEAR, 9, 23)
+TALK_DAY_2 = datetime.date(CONFERENCE_YEAR, 9, 24)
+TALK_DAY_3 = datetime.date(CONFERENCE_YEAR, 9, 25)
+SPRINTS_DAY_1 = datetime.date(CONFERENCE_YEAR, 9, 26)
+SPRINTS_DAY_2 = datetime.date(CONFERENCE_YEAR, 9, 27)
+
+SMALL_TALK_ROOM = "Grand Ballroom III"
+LARGE_TALK_ROOM = "Junior Ballroom"
+
+LUNCH_ROOM = "Grand Ballroom I/II"
+
+LACTATION_ROOM = "Meeting Room 1"
+QUIET_ROOM = "Meeting Room 4"
+GREEN_ROOM = "Meeting Room 2"
+
+TUTORIAL_TRACK_A_ROOM = "Tutorial Track A (TBD)"
+TUTORIAL_TRACK_B_ROOM = "Tutorial Track B (TBD)"
+TUTORIAL_TRACK_C_ROOM = "Tutorial Track C (TBD)"
